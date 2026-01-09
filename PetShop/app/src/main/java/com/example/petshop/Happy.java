076527131e5075bd43a1f578487d6f1fb5015e4c
bbhall <bbhall@ualberta.ca>
2026-01-09 09:09:24 -0700

    Completed Lab Demo

--- PetShop/app/src/main/java/com/example/petshop/Happy.java
+++ PetShop/app/src/main/java/com/example/petshop/Happy.java
@@ -11,6 +11,7 @@ public class Happy extends Mood {
         super(date);
     }
 
+    @Override
     public String moodDescription() {
         return "I am Happy! :)";
     }
--- PetShop/app/src/main/java/com/example/petshop/Sad.java
+++ PetShop/app/src/main/java/com/example/petshop/Sad.java
@@ -11,6 +11,7 @@ public class Sad extends Mood {
         super(date);
     }
 
+    @Override
     public String moodDescription() {
         return "I am Sad... :(";
     }
